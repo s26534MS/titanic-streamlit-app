0fabf24481d7aeef08a5e893001085aacb814ff3
Miłosz Sierakowski <roland_m1@wp.pl>
2025-03-30 12:46:12 +0200

    first commit

---
+++ datasets/trenowanieML.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+import pickle
+
+# Wczytanie danych
+base_data = pd.read_csv("DSP_1.csv")
+
+# Wybór kolumn
+cols = ["Survived", "Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
+data = base_data[cols].copy()
+
+# Uzupełnienie brakujących wartości wieku (usuwa FutureWarning)
+data.loc[:, "Age"] = data["Age"].fillna(data["Age"].mean())
+
+# Usunięcie braków w 'Embarked'
+data.dropna(subset=['Embarked'], inplace=True)
+
+# Zakodowanie zmiennych kategorycznych
+encoder = LabelEncoder()
+data.loc[:, "Sex"] = encoder.fit_transform(data.loc[:, "Sex"])
+data.loc[:, "Embarked"] = encoder.fit_transform(data.loc[:, "Embarked"])
+
+# Podział na cechy (X) i etykiety (y)
+y = data.iloc[:, 0]
+X = data.iloc[:, 1:8]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+
+# Funkcja trenująca model
+def train_model(X_train, y_train):
+    forest = RandomForestClassifier(n_estimators=20, random_state=0)
+    forest.fit(X_train, y_train)
+    print("Dokładność modelu:", forest.score(X_train, y_train))
+    return forest
+
+
+# Trenujemy model
+forest = train_model(X_train, y_train)
+
+# Przykładowe dane testowe (poprawiony format)
+my_data = pd.DataFrame([
+    [1, 1, 50, 0, 0, 0, 2]
+], columns=X.columns)
+
+# Predykcja (usuwa UserWarning)
+print(forest.predict(my_data))
+
+# Kolejna próbka
+my_data = np.array([
+    [1, 0, 20, 1, 0, 0, 2]
+])
+
+# Predykcja
+print(forest.predict(my_data))
+
+# Zapis modelu
+filename = "model.sv"
+pickle.dump(forest, open(filename, "wb"))
---
+++ materiały/app_broken.py
@@ -0,0 +1,64 @@
+# źródło danych [https://www.kaggle.com/c/titanic/](https://www.kaggle.com/c/titanic)
+
+import pandas as pd
+import streamlit as st
+import pickle
+from datetime import datetime
+
+startTime = datetime.now()
+# import znanych nam bibliotek
+
+import pathlib
+from pathlib import Path
+
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+filename = "../datasets/model.sv"
+model = pickle.load(open(filename, 'rb'))
+# otwieramy wcześniej wytrenowany model
+
+pclass_d = {0: "Pierwsza", 1: "Druga", 2: "Trzecia"}
+embarked_d = {0: "Cherbourg", 1: "Queenstown", 2: "Southampton"}
+sex_d = {0: "Kobieta", 1: "Mężczyzna"}
+
+
+# o ile wcześniej kodowaliśmy nasze zmienne, to teraz wprowadzamy etykiety z ich nazewnictwem
+
+def main():
+    st.set_page_config(page_title="Titanic - Przewidywanie Przeżycia")
+    overview = st.container()
+    left, right = st.columns(2)
+    prediction = st.container()
+
+    st.image("https://snipboard.io/GCcVZD.jpg")
+
+    with overview:
+        st.title("Titanic - Przewidywanie Przeżycia")
+
+    with left:
+        pclass_radio = st.radio("Klasa pasażera", list(pclass_d.keys()), format_func=lambda x: pclass_d[x])
+        sex_radio = st.radio("Płeć", list(sex_d.keys()), format_func=lambda x: sex_d[x])
+        embarked_radio = st.radio("Port zaokrętowania", list(embarked_d.keys()), index=2,
+                                  format_func=lambda x: embarked_d[x])
+
+    with right:
+        age_slider = st.slider("Wiek", value=1, min_value=1, max_value=80)
+        sibsp_slider = st.slider("Liczba rodzeństwa i/lub partnera", min_value=0, max_value=8)
+        parch_slider = st.slider("Liczba rodziców i/lub dzieci", min_value=0, max_value=6)
+        fare_slider = st.slider("Cena biletu", min_value=0, max_value=512, step=1)
+
+    data = pd.DataFrame(
+        [[pclass_radio, sex_radio, age_slider, sibsp_slider, parch_slider, fare_slider, embarked_radio]],
+        columns=["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"])
+    survival = model.predict(data)
+    s_confidence = model.predict_proba(data)
+
+    with prediction:
+        st.subheader("Czy taka osoba przeżyłaby katastrofę?")
+        st.subheader(("Tak" if survival[0] == 1 else "Nie"))
+        st.write("Pewność predykcji {0:.2f} %".format(s_confidence[0][survival][0] * 100))
+
+
+if __name__ == "__main__":
+    main()
